3114d654d583bfa265864bd57f6ef46678aa94bb
aleenakuruvilla <aleenavaithra@gmail.com>
2024-05-02 09:46:12 +0530

    blog chn

--- Website/Flash/frontend/src/Contact.jsx
+++ Website/Flash/frontend/src/Contact.jsx
@@ -1,7 +1,18 @@
+import { CollectionReference } from 'firebase/firestore'
 import React from 'react'
+
+import './contact.css';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className="contactcss">
+   
+  <h1>Contact</h1>
+    <p>Aditya Manoj</p>
+    <p>Aleena Kuruvila</p>
+    <p>Alfin Jose</p>
+    <h2>contactflashcardapp@gmailcom</h2>
+    </div>
+
   )
 }
